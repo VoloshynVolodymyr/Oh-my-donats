b810d89348a79c4a0b4249c0b52c5befe0b1ac3d
VoloshynVolodymyr <111738772+VoloshynVolodymyr@users.noreply.github.com>
2022-10-03 19:55:49 +0000

    Deploying to gh-pages from @ VoloshynVolodymyr/Oh-my-donats@96853f618af8ca71e78a31af19a737a8568a30ac 🚀

---
+++ index.2a1389b3.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.body.classList.add("loaded_hiding"),window.setTimeout((function(){document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")}),2e3)};
+//# sourceMappingURL=index.2a1389b3.js.map
